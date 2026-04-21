0a76be37ee6797a1d9a4b024c519797a8a81cff8
ChaithanyaJ04 <chaithanyaj.cs25@bmsce.ac.in>
2026-04-21 21:57:36 +0530

    Add files via upload

---
+++ Quicksort/Quick_sort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int partition(int a[], int low, int high)
+{
+    int pivot = a[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
+
+    while (1)
+    {
+        while (i <= high && a[i] < pivot)
+            i++;
+
+        while (j >= low && a[j] > pivot)
+            j--;
+
+        if (i < j)
+        {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    temp = a[low];
+    a[low] = a[j];
+    a[j] = temp;
+
+    return j;
+}
+
+void quicksort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int p = partition(a, low, high);
+        quicksort(a, low, p - 1);
+        quicksort(a, p + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    quicksort(a, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
